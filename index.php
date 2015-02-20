bf146a9ba0100712ab312a1d0fbcf9245134e2f2
lordmatt <lordmatt@Hope>
2015-02-20 03:44:52 +0000

    Demonstrated working (with warnings sadly)

--- index.php
+++ index.php
@@ -7,20 +7,57 @@
     <body>
         <h1>Demo of system in progress.</h1>
         
-        <pre>
+        <pre>Before we start we get things booted up
+
         <?php
         
+                
         require_once("./UER/a/aLogger.php");
         require_once("./UER/EntityProcessor.php");
         $EP = new EntityProcessor();
         
+        
+        ?>
+
+Now we take a URL [http://lordmatt.co.uk] and process it:
+
+        <?php
+
+        
         $lm = $EP->getObject('http://lordmatt.co.uk');
         
-        var_dump($lm);
+        echo "\n";
         if($lm){
-            echo $lm->render_class();
+            echo "result time: ", $lm->render_class();
         }else{
+            echo "lm:";
+            var_dump($lm);
+            echo "<br />error log";
             var_dump($EP->get_error_log());
+            echo "<br />";
+            echo "<br />EP";
+            var_dump($EP);
+        }        
+        ?>
+<br />
+So that was pretty limp but it shows that the object system is working.
+
+Now we try a URN - URN:ASIN:1844137902<br />
+        <?php
+        
+        $book = $EP->getObject('URN:ASIN:1844137902');
+        
+        echo "\n";
+        if($book){
+            echo "result time: ", $book->render_class();
+        }else{
+            echo "book:";
+            var_dump($book);
+            echo "<br />error log";
+            var_dump($EP->get_error_log());
+            echo "<br />";
+            echo "<br />EP";
+            var_dump($EP);
         }        
         ?>
         </pre>
