49c97f8dbdd243357f3d49c3c314d449e122f140
lordmatt <lordmatt@Hope>
2015-02-20 02:48:12 +0000

    Early Development towards something that works

--- index.php
+++ index.php
@@ -6,5 +6,23 @@
     </head>
     <body>
         <h1>Demo of system in progress.</h1>
+        
+        <pre>
+        <?php
+        
+        require_once("./UER/a/aLogger.php");
+        require_once("./UER/EntityProcessor.php");
+        $EP = new EntityProcessor();
+        
+        $lm = $EP->getObject('http://lordmatt.co.uk');
+        
+        var_dump($lm);
+        if($lm){
+            echo $lm->render_class();
+        }else{
+            var_dump($EP->get_error_log());
+        }        
+        ?>
+        </pre>
     </body>
 </html>
