87ce2843c681f5a414a63f4fcab799da2e654de9
lordmatt <lordmatt@Hope>
2015-02-22 06:46:07 +0000

    added boilerplate message to comments.

--- UER/URX.php
+++ UER/URX.php
@@ -1,6 +1,23 @@
 <?php
 /** 
   * The purpose of this file is to make your life slightly easier.
+ * 
+ * @author Lord Matt 
+ * @link https://github.com/lordmatt/URX
+ * 
+ * @license
+ *  Copyright (C) 2015 Matthew Brown
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
   */
     $path_to_files = dirname(__FILE__);
     require_once("{$path_to_files}/a/aLogger.php");
